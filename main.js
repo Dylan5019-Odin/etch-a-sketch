4bbdaa66d509e207b0fd6087db2ba1bfae132e87
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-02 17:11:36 -0600

    Added the ability for the user to set the grid size

--- main.js
+++ main.js
@@ -2,26 +2,42 @@
 //Retrieve the div that contains the grid
 let grid = document.querySelector(`.grid`);
 
-//The variable that controls the height and width of the grid
-let gridSize = 20;
 
-
-//A for loop that add the individual divs to the grid
-for (let i = 0; i < 400; i++) {
-    let temp = document.createElement(`div`);
-    temp.id = `grid-box`;
-    grid.appendChild(temp);   
-}
-
-//Adding a style to the grid element that allows it to dynamically change its dimensions
-grid.style = `grid-template-columns: repeat(${gridSize}, 1fr);`;
-
-
-//Retrieving the div elements with the class grod-box and setting a event listener for mouse enter
-//that add a class to the grid element chaning its background color
-let gridBoxes = document.querySelectorAll("#grid-box").forEach(box => {
-    box.addEventListener("mouseenter", () => {box.classList.add("blue")});
+//Retrieve the that controls the grid size from the HTML
+//Add an event listener to the button that prompts the user to enter
+//a new grid size, With a limit of 100
+document.getElementById('sizeButton').addEventListener("click", () => {
+    let newSize = prompt("Enter a new grid size, Max size allowed: 100");
+    newSize <= 100 ? generateGrid(newSize) : generateGrid(100);
+    console.log(newSize);
 });
 
 
+//A function that generates a new grid using the provided size
+const generateGrid = (gridSize) => {
 
+    clearGrid();
+  //A for loop that add the individual divs to the grid
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let temp = document.createElement(`div`);
+    temp.id = `grid-box`;
+    grid.appendChild(temp);
+  }
+
+  //Adding a style to the grid element that allows it to dynamically change its dimensions
+  grid.style = `grid-template-columns: repeat(${gridSize}, 1fr);`;
+
+  //Retrieving the div elements with the class grod-box and setting a event listener for mouse enter
+  //that add a class to the grid element chaning its background color
+  let gridBoxes = document.querySelectorAll("#grid-box").forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.classList.add("blue");
+    });
+  });
+};
+
+
+//A function that clears the grid element of all its children
+const clearGrid = () => {
+  grid.replaceChildren();
+}
