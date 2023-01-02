9a6059e4486bc416fa567de9f9278e826c694084
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-02 16:56:57 -0600

    Changed the code so that the grid can dynamicaly change size

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
+
+//Retrieve the div that contains the grid
 let grid = document.querySelector(`.grid`);
 
-for (let i = 0; i < 500; i++) {
+//The variable that controls the height and width of the grid
+let gridSize = 20;
+
+
+//A for loop that add the individual divs to the grid
+for (let i = 0; i < 400; i++) {
     let temp = document.createElement(`div`);
     temp.id = `grid-box`;
     grid.appendChild(temp);   
 }
 
+//Adding a style to the grid element that allows it to dynamically change its dimensions
+grid.style = `grid-template-columns: repeat(${gridSize}, 1fr);`;
 
 
+//Retrieving the div elements with the class grod-box and setting a event listener for mouse enter
+//that add a class to the grid element chaning its background color
 let gridBoxes = document.querySelectorAll("#grid-box").forEach(box => {
     box.addEventListener("mouseenter", () => {box.classList.add("blue")});
 });
 
-console.log(gridBoxes);
+
 
